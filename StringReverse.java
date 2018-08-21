2b7c5a28eacbb4d62a5e5f2612ad2e54bb43039f
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-21 11:23:37 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class te {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.nextLine();
+	StringBuilder sb=new StringBuilder();
+	System.out.println(sb.append(s).reverse().toString());
+}
+}
